29b97023858c11d005b71b77850af4c45437c7ac
isakilikya <isakilikya@gmail.com>
2021-12-25 11:05:33 +0300

    initialize spring-bootcamp-week1-hw2

---
+++ Hafta-1/odev-2/spring-bootcamp-week1-hw2/src/main/java/com/example/springbootcampweek1hw2/SpringBootcampWeek1Hw2Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootcampweek1hw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootcampWeek1Hw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootcampWeek1Hw2Application.class, args);
+	}
+
+}
---
+++ Hafta-1/odev-2/spring-bootcamp-week1-hw2/src/test/java/com/example/springbootcampweek1hw2/SpringBootcampWeek1Hw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootcampweek1hw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootcampWeek1Hw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
